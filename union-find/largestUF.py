884d904d096dbe23b1145a7de6ce698582965357
flacode <flavianshemerirwe@gmail.com>
2017-02-17 19:00:06 +0300

    largest element

--- union-find/largestUF.py
+++ union-find/largestUF.py
@@ -1,4 +1,11 @@
-"""Returning the largest element from a component containing p"""
+"""Returning the largest element from a component containing p
+   Successor with delete. Given a set of N integers S={0,1,...,N−1}
+and a sequence of requests of the following form:
+• Remove x from S
+• Find the successor of x: the smallest y in S such that y ≥ x.
+Design a data type so that all operations (except construction) should take
+logarithmic time or better. 
+"""
 class LargestItemComponent:
 	def __init__(self, N):
 		self.count=N
