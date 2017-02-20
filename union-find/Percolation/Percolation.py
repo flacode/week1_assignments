6c5b66416158ac94a763d31f286ba14214f415b8
flacode <flavianshemerirwe@gmail.com>
2017-02-20 19:22:58 +0300

    comments

--- union-find/Percolation/Percolation.py
+++ union-find/Percolation/Percolation.py
@@ -1,44 +1,16 @@
-#class for weighted union find
+from unionFind import WeightedUnionFind #import unionFind class
 #class for Percolation
-
-
-class WeightedUnionFind:
-	def __init__(self, N):
-		self.count=N
-		self.id=list(range(N))
-		self.sz=[1]*N
-
-	def root(self, p):
-		while p != self.id[p]:
-			self.id[p]=self.id[self.id[p]]
-			p=self.id[p]
-		return p
-
-	def union(self, p, q):
-		Rp=self.root(p)
-		Rq=self.root(q)
-		if Rp == Rq:
-			return
-		if self.sz[Rp] < self.sz[Rq]:
-			self.id[Rp]=Rq
-			self.sz[Rq]=self.sz[Rq]+self.sz[Rp]
-		else:
-			self.id[Rq]=Rp
-			self.sz[Rp]=self.sz[Rp]+self.sz[Rq]
-		self.count=self.count-1
-
-	def connected(self, p,q):
-		return self.root(p)==self.root(q)
 
 			
 class Percolation:
 	def __init__(self, grid):
-		self.rows=len(grid)
-		self.cols=len(grid[0])
-		self.grid_size=self.rows*self.cols
-		self.top=self.grid_size
-		self.bottom=self.grid_size+1
+		self.rows=len(grid)#number of rows in grid
+		self.cols=len(grid[0])#number of columns in grid
+		self.grid_size=self.rows*self.cols#number of cells in the entire grid
+		self.top=self.grid_size #virtual node to represent top
+		self.bottom=self.grid_size+1 #virtual node to represent bottom
 		self.grid=grid
+		"""Mark cells in a virtual grid with numbers from 0 to gridsize-1""" 
 		v=0
 		self.vgrid=[]
 		for i in range(self.rows):
@@ -49,11 +21,11 @@ class Percolation:
 		self.uf=WeightedUnionFind(self.grid_size+2) # 2 for the two extra nodes for top and bottom
 
 
-	"""If cell is valid and it is open"""
+	"""If cell is valid and it is open, check if any of its neighbors are open and connect the two cells"""
 	def check_neighbors(self, i, j):
-		if i==0:
+		if i==0:#if the cell is in the top row, connect it to the virtual top 
 			self.uf.union(self.vgrid[i][j], self.top)
-		if i==self.rows-1:
+		if i==self.rows-1:#if cell is in the last row, connect it to the virtual bottom
 			self.uf.union(self.vgrid[i][j], self.bottom)
 		if i-1>=0 and self.grid[i-1][j]=='1':
 			self.uf.union(self.vgrid[i][j], self.vgrid[i-1][j])
@@ -64,13 +36,14 @@ class Percolation:
 		if j+1 < self.cols and self.grid[i][j+1]=='1':
 			self.uf.union(self.vgrid[i][j], self.vgrid[i][j-1])
 
+	"""Check the grid for open cells and connect them to their neighbors"""	
 	def discover(self):
 		for i in range(self.rows):
 			for j in range(self.cols):
 				if self.grid[i][j]=='1':
 					self.check_neighbors(i, j)
 					
-
+	"""Check if the virtual top is connected to the virtual bottom"""
 	def percolates(self):
 		if self.uf.connected(self.top, self.bottom):
 			print ("It Percolates!!!")
